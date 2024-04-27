d3b2f0673fd53fdd380cfe6ff0cc6eaf3855ba2d
Aleksandr Ermolov <alermolo@z4r3p1.42lyon.fr>
2024-04-27 14:43:43 +0200

    added merge checks

---
+++ srcs/merge_check.c
@@ -0,0 +1,134 @@
+#include "wkw.h"
+
+int	merge_check_right(int board[4][4])
+{
+	int	i;
+	int	j;
+	int	k;
+	int	can_merge = 0;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 3;
+		while (j > 0)
+		{
+			if (board[i][j] != 0)
+			{
+				k = j - 1;
+				while (k >= 0)
+				{
+					if (board[i][k] != 0)
+					{
+						if (board[i][j] == board[i][k])
+							can_merge++;
+					}
+					k--;
+				}
+			}
+			j--;
+		}
+		i++;
+	}
+	return (can_merge);
+}
+
+int	merge_check_left(int board[4][4])
+{
+	int	i;
+	int	j;
+	int	k;
+	int	can_merge = 0;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 3)
+		{
+			if (board[i][j] != 0)
+			{
+				k = j + 1;
+				while (k < 4)
+				{
+					if (board[i][k] != 0)
+					{
+						if (board[i][j] == board[i][k])
+							can_merge++;
+					}
+					k++;
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	return (can_merge);
+}
+
+
+int	merge_check_up(int board[4][4])
+{
+	int	i;
+	int	j;
+	int	k;
+	int	can_merge = 0;
+
+	j = 0;
+	while (j < 4)
+	{
+		i = 0;
+		while (i < 3)
+		{
+			if (board[i][j] != 0)
+			{
+				k = i + 1;
+				while (k < 4)
+				{
+					if (board[k][j] != 0)
+					{
+						if (board[i][j] == board[k][j])
+							can_merge++;
+					}
+					k++;
+				}
+			}
+			i++;
+		}
+		j++;
+	}
+	return (can_merge);
+}
+
+int	merge_check_down(int board[4][4])
+{
+	int	i;
+	int	j;
+	int	k;
+	int	can_merge = 0;
+
+	j = 0;
+	while (j < 4)
+	{
+		i = 3;
+		while (i > 0)
+		{
+			if (board[i][j] != 0)
+			{
+				k = i - 1;
+				while (k >= 0)
+				{
+					if (board[k][j] != 0)
+					{
+						if (board[i][j] == board[k][j])
+							can_merge++;
+					}
+					k--;
+				}
+			}
+			i--;
+		}
+		j++;
+	}
+	return (can_merge);
+}
