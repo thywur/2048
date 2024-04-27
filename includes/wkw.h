74bddba93c2760f2a3ae7e67c93fb921d7038998
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-04-27 12:47:45 +0200

    truc

--- includes/wkw.h
+++ includes/wkw.h
@@ -5,6 +5,25 @@
 #include <wchar.h>
 #include <locale.h>
 
+#define WIDTH 800
+#define HEIGHT 600
+
+typedef struct env_4
+{
+	int filled;
+	int board[4][4];
+	int size;
+}	t_env_4;
+
+typedef struct env_5
+{
+	int filled;
+	int board[5][5];
+	int size;
+}	t_env_5;
+
+
+
 int init_ncurses(WINDOW *stdscr);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,16 +1,67 @@
 #include "wkw.h"
 
+#include <stdlib.h>
+#include <time.h>
+
 # include <unistd.h>
+
+void	print_board(int board[4][4])
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			printf("%d ", board[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+}
 
 int	main(void)
 {
-	WINDOW *win = NULL;
+	// WINDOW *win = NULL;
 
-	if (init_ncurses(win))
+	// if (init_ncurses(win))
+	// {
+	// 	return (1);
+	// }
+	// usleep(5000000);
+	// endwin();
+	int	x;
+	int	y;
+	int filled;
+
+	int	board[4][4] = {
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0}
+	};
+
+	srand(time(NULL));
+
+	filled = 0;
+	while (filled < 2)
 	{
-		return (1);
+		x = rand() % 4;
+		y = rand() % 4;
+		if (board[x][y] == 0)
+		{
+			if (rand() % 10 < 9)
+				board[x][y] = 2;
+			else
+				board[x][y] = 4;
+			filled++;
+		}
 	}
-	usleep(5000000);
-	endwin();
+
+	print_board(board);
 	return (0);
 }
