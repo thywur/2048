f77690b8c36d86a7ede494891959ba1cc4cfca1c
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-04-28 15:47:30 +0200

    Remove define

--- srcs/score.c
+++ srcs/score.c
@@ -1,4 +1,4 @@
-#include "../includes/wkw.h"
+#include "wkw.h"
 
 #include <unistd.h>
 #include <stdio.h>
@@ -7,29 +7,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
-
-# define SIZE 4
-
-// int	count_score(int board[SIZE][SIZE])
-// {
-// 	int	i;
-// 	int	j;
-// 	int	score;
-
-// 	i = 0;
-// 	score = 0;
-// 	while (i < SIZE)
-// 	{
-// 		j = 0;
-// 		while (j < SIZE)
-// 		{
-// 			score += board[i][j];
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// 	return (score);
-// }
 
 int	save_score(int score, char *name, int size)
 {
@@ -43,59 +20,6 @@ int	save_score(int score, char *name, int size)
 	close(fd);
 	return (0);
 }
-
-// void	sort_tab(char **tab, size_t size)
-// {
-// 	size_t	i;
-// 	int		tmp;
-
-// 	i = 0;
-// 	while (i < size && i + 1 < size)
-// 	{
-// 		if (tab[i] < tab[i + 1])
-// 		{
-// 			tmp = tab[i];
-// 			tab[i] = tab[i + 1];
-// 			tab[i + 1] = tmp;
-// 			i = 0;
-// 		}
-// 		else
-// 			i++;
-// 	}
-// }
-
-// void	sort_scores(void)
-// {
-// 	int		fd;
-// 	int		*tab;
-// 	size_t	i;
-// 	int		tmp;
-// 	size_t	size;
-
-// 	fd = open("highscores.txt", O_RDWR | O_CREAT, 0644);
-// 	if (fd == -1)
-// 		return ;
-// 	size = 0;
-// 	while (read(fd, &tmp, sizeof(int)))
-// 		size++;
-// 	tab = malloc(sizeof(int) * size);
-// 	if (!tab)
-// 		return ;
-// 	lseek(fd, 0, SEEK_SET);
-// 	i = 0;
-// 	while (read(fd, &tab[i], sizeof(int)))
-// 		i++;
-// 	close(fd);
-// 	sort_tab(tab, size);
-// 	fd = open("highscores.txt", O_RDWR | O_CREAT | O_TRUNC, 0644);
-// 	if (fd == -1)
-// 		return ;
-// 	i = 0;
-// 	while (i < size)
-// 		dprintf(fd, "%d\n", tab[i++]);
-// 	close(fd);
-// 	free(tab);
-// }
 
 int	ft_compare(const void *a, const void *b)
 {
@@ -114,56 +38,6 @@ void	free_tab(char **tab)
 	}
 	free(tab);
 }
-
-// int	sort_scores(void)
-// {
-// 	FILE	*fd;
-// 	char	**tab;
-// 	size_t	i;
-// 	size_t	size;
-// 	char	line[100];
-// 	int		ret;
-
-// 	fd = fopen("highscores.txt", "r");
-// 	if (!fd)
-// 		return (1);
-// 	size = 0;
-// 	while (fgets(line, 100, fd))
-// 		size++;
-// 	if (fseek(fd, 0, SEEK_SET) == -1)
-// 		return (1);
-// 	tab = malloc(sizeof(char *) * (size + 1));
-// 	if (!tab)
-// 		return (1);
-// 	memset(tab, 0, sizeof(char *) * (size + 1));
-// 	i = 0;
-// 	while (fgets(line, 100, fd))
-// 	{
-// 		tab[i] = strdup(line);
-// 		if (!tab[i])
-// 		{
-// 			free_tab(tab);
-// 			return (1);
-// 		}
-// 		i++;
-// 	}
-// 	tab[i] = NULL;
-// 	fclose(fd);
-// 	qsort(tab, size, sizeof(char *), ft_compare);
-// 	fd = fopen("highscores.txt", "w+");
-// 	if (!fd)
-// 	{
-// 		free_tab(tab);
-// 		return (1);
-// 	}
-// 	ret = 0;
-// 	i = 0;
-// 	while (i < size && ret != -1)
-// 		ret = fprintf(fd, "%s", tab[i++]);
-// 	fclose(fd);
-// 	free_tab(tab);
-// 	return (ret == -1);
-// }
 
 void	sort_scores(char **scores)
 {
